a3e708112f5c0603437ae1a8e9c3ee06cde5c8f8
Konstantin Martynenko <konstantin.vents@gmail.com>
2023-02-28 17:06:17 +0200

    02_video__commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryListItemsArray(galleryItems) {
+    
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>
+            `
+        });
+    // .join('')
+};
+
+const galleryItemsArray = createGalleryListItemsArray(galleryItems);
+// console.log(galleryItemsArray);
+
+galleryItemsArray.forEach(el => {
+    const galleryListItem = document.createElement('li');
+    galleryListItem.innerHTML = el;
+    galleryList.append(galleryListItem);
+});
+
+console.log(galleryList);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionType: "attr", captionsData: "alt" });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+// import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+function onPlay({ seconds }) {
+    // data is an object containing properties specific to that event
+
+    // player.setCurrentTime("videoplayer-current-time", seconds);
+
+    localStorage.setItem("videoplayer-current-time", seconds);
+    console.log(localStorage)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+
 
